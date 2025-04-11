72c640438b9a3be5514a58c7b376c1ef0532ebcf
Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>
2025-04-11 00:53:52 -0400

    google doc fix
    Signed-off-by: Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 
 export default defineConfig({
+  output: "server", // Enable SSR mode
   site: "https://lpcvai.github.io",
 });
