1fadf2784b5441140e83576087fabad5ab6b69a9
Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>
2025-04-11 01:41:13 -0400

    remove ssr
    Signed-off-by: Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 
 export default defineConfig({
-  // Enable SSR mode
-  output: "server",
-
   site: "https://lpcvai.github.io",
-  integrations: [react()],
 });
